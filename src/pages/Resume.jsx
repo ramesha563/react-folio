bb3d9316d16c3ba4c3e8d46e3f0f3fb449529abc
ramesha563 <113997079+ramesha563@users.noreply.github.com>
2025-08-21 00:00:06 +0500

    Update Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -52,7 +52,7 @@ const Resume = () => {
       <div className="resume-container">
         <h2 className="resume-title">Ramesha's Resume</h2>
         <p className="resume-text">Here you can view or download my resume to learn more about my qualifications, skills, and experiences:</p>
-        <a href="/Rameshacv.pdf" download className="resume-button">
+        <a href="/Ramesha CV Resume (1).pdf" download className="resume-button">
           <FaDownload className="download-icon" /> Download Resume
         </a>
       </div>
